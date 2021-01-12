ab8b4c779b67b1a69ef05ee4eec41d62e291552a
Sanjay Prajapati <sanjay270899@gmail.com>
2021-01-12 22:32:03 +0530

    SignIn SignUp in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header/Header';
+import SignIn from './components/Auth/SignIn/SignIn';
+import SignUp from './components/Auth/SignUp/SignUp';
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
     <div className="App">
     <Router>
       <Header />
+      <SignIn />
+      <SignUp />
     </Router>
     </div>
   );
